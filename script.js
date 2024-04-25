fade41cf59829864f8eb603e0b0567f8d1e616dd
Mariam Sadek <105167781+mariamnageh@users.noreply.github.com>
2024-04-25 19:37:24 +0200

    Add user icon with click event to redirect to login page; update font and icon links; add script for password visibility toggle
    The changes include:
    
    * Adding a user icon element in the HTML with a click event to redirect to the

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");
 
@@ -16,3 +15,48 @@ function closeSidebar() {
     sidebarOpen = false;
   }
 }
+
+
+
+
+ const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the user icon element
+    var userIcon = document.getElementById("user-icon");
+
+    // Add click event listener to the user icon
+    userIcon.addEventListener("click", function() {
+        // Redirect the user to the login.html page
+        window.location.href = "login.html";
+    });
+});
